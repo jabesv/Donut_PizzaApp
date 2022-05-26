9a97b2d42b140469990c450a8e5178c2796e7ee2
learner-22 <nbhooma@gmail.com>
2022-05-26 15:49:24 -0400

    added script for fetch

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 
 
 
-fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
+const manhattan = document.getElementById('manhattan')
+
+const complaints = document.getElementById('complaints')
+ let borough ='MANHATTAN'
+ let limit = 10
+ let allInfo =''
+fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough='+borough + '&$limit=' +limit)
 .then(response => response.json()) //parse the response (res) object to json
-.then(data => console.log(data)) // json data
-.catch(err => console.log(err)) // handling errors
+//.then(data =>console.log(data))
+.then(data => complaints.innerHTML= data[0].descriptor )
+
+//.then(data => data => allInfo(data)) // json data
+.catch(err => console.log(err))
+
+console.log(allInfo)
